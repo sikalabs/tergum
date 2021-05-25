ed724f6de1389c34a2d17577ca3ede1ff2f33576
Ondrej Sika <ondrej@ondrejsika.com>
2021-05-25 23:04:18 +0200

    feat(misc/docker/mysql): Create simple mysql server in Docker

---
+++ misc/docker/mysql/Dockerfile
@@ -0,0 +1,2 @@
+FROM mysql
+COPY init.sql /docker-entrypoint-initdb.d/
---
+++ misc/docker/mysql/init.sql
@@ -0,0 +1,26 @@
+CREATE TABLE example_users (
+  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(32) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE example_messages (
+  id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  text VARCHAR(128) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO example_users
+VALUES
+  (1, 'ondrejsika'),
+  (2, 'alice'),
+  (3, 'bob')
+;
+
+INSERT INTO example_messages
+VALUES
+  (1, 1, 'Hello World!'),
+  (2, 1, 'Ahoj Svete!'),
+  (3, 1, 'Hallo Welt!')
+;
