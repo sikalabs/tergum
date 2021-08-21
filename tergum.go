131ecf1ac87fcd23f9b15660d7459dc547cdf309
Ondrej Sika <ondrej@ondrejsika.com>
2021-08-21 02:18:07 +0200

    feat: Add -v for print version

--- tergum.go
+++ tergum.go
@@ -3,18 +3,26 @@ package main
 import (
 	"errors"
 	"flag"
+	"fmt"
 	"log"
 
 	"github.com/sikalabs/tergum/tergum1"
 	"github.com/sikalabs/tergum/tergum2"
+	"github.com/sikalabs/tergum/version"
 )
 
 func main() {
 	// Backup parameters from config file
 	path := flag.String("config", "", "tergum config file (json)")
 	v2 := flag.Bool("v2", false, "tergum v2")
+	ver := flag.Bool("v", false, "show tergum version")
 
 	flag.Parse()
+
+	if *ver {
+		fmt.Println(version.Version)
+		return
+	}
 
 	if *path == "" {
 		log.Fatal(errors.New("tergum require config file (-config)"))
