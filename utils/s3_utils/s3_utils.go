cd46f4ceb5764c028910b2412ba777aeea886f97
Ondrej Sika <ondrej@ondrejsika.com>
2021-10-01 15:42:33 +0200

    feat(utils/s3_utils): Add s3_utils.Upload to utils

---
+++ utils/s3_utils/s3_utils.go
@@ -0,0 +1,53 @@
+package s3_utils
+
+import (
+	"io"
+
+	aws_aws "github.com/aws/aws-sdk-go/aws"
+	aws_credentials "github.com/aws/aws-sdk-go/aws/credentials"
+	aws_session "github.com/aws/aws-sdk-go/aws/session"
+	aws_s3manager "github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
+
+func Upload(
+	access_key string,
+	secret_key string,
+	region string,
+	endpoint string,
+	bucket_name string,
+	key string,
+	f io.Reader,
+) error {
+	awsConfig := aws_aws.Config{
+		Credentials: aws_credentials.NewStaticCredentials(
+			access_key,
+			secret_key,
+			"",
+		),
+	}
+	if region != "" {
+		awsConfig.Region = aws_aws.String(region)
+	}
+	if endpoint != "" {
+		awsConfig.Region = aws_aws.String(string("us-east-1"))
+		awsConfig.S3ForcePathStyle = aws_aws.Bool(true)
+		awsConfig.Endpoint = aws_aws.String(endpoint)
+	}
+	session, err := aws_session.NewSession(
+		&awsConfig,
+	)
+	if err != nil {
+		return err
+	}
+	uploader := aws_s3manager.NewUploader(session)
+	_, err = uploader.Upload(&aws_s3manager.UploadInput{
+		Bucket: aws_aws.String(bucket_name),
+		ACL:    aws_aws.String("private"),
+		Key:    aws_aws.String(key),
+		Body:   f,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
