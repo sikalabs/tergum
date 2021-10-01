7a6c203a5477e243e7234161a2140b5977174872
Ondrej Sika <ondrej@ondrejsika.com>
2021-10-01 15:04:38 +0200

    feat(backup/source/single_file): Add SingleFileSource

---
+++ backup/source/single_file/single_file.go
@@ -0,0 +1,22 @@
+package single_file
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+type SingleFileSource struct {
+	Path string `yaml:"Path"`
+}
+
+func (s SingleFileSource) Validate() error {
+	if s.Path == "" {
+		return fmt.Errorf("SingleFileSource need to have a FilePath")
+	}
+	return nil
+}
+
+func (s SingleFileSource) Backup() ([]byte, error) {
+	out, err := ioutil.ReadFile(s.Path)
+	return out, err
+}
