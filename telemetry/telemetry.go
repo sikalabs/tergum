841dae4f907a610fe2c1ddb911c035b6168f1e4a
Ondrej Sika <ondrej@ondrejsika.com>
2021-10-09 11:24:36 +0200

    feat(telemetry): Add optional host data collection (only hostname yet, diable by default)

--- telemetry/telemetry.go
+++ telemetry/telemetry.go
@@ -1,6 +1,7 @@
 package telemetry
 
 import (
+	"os"
 	"time"
 
 	"github.com/go-resty/resty/v2"
@@ -11,15 +12,21 @@ import (
 const DEFAULT_TELEMETRY_ORIGIN = "https://tergum-telemetry-api.sikalabs.com"
 
 type TelemetryConfig struct {
-	Origin  string `yaml:"Origin"`
-	Disable bool   `yaml:"Disable"`
-	Name    string `yaml:"Name"`
+	Origin          string `yaml:"Origin"`
+	Disable         bool   `yaml:"Disable"`
+	Name            string `yaml:"Name"`
+	CollectHostData bool   `yaml:"CollectHostData"`
+}
+
+type HostData struct {
+	Hostname string `yaml:"Hostname"`
 }
 
 type Telemetry struct {
-	Config  TelemetryConfig
-	Enabled bool
-	Client  *resty.Client
+	Config   TelemetryConfig
+	Enabled  bool
+	Client   *resty.Client
+	HostData *HostData
 }
 
 func NewTelemetry(tc *TelemetryConfig, disabled bool, extraName string) Telemetry {
@@ -52,11 +59,27 @@ func NewTelemetry(tc *TelemetryConfig, disabled bool, extraName string) Telemetr
 		Str("version", version.Version).
 		Msg("Telemetry backend initialized.")
 
-	return Telemetry{
-		Enabled: true,
-		Config:  *tc,
-		Client:  client,
+	var hostData *HostData
+	if tc.CollectHostData {
+		hostname, _ := os.Hostname()
+		hostData = &HostData{
+			Hostname: hostname,
+		}
 	}
+
+	return Telemetry{
+		Enabled:  true,
+		Config:   *tc,
+		Client:   client,
+		HostData: hostData,
+	}
+}
+
+func (t Telemetry) GetHostname() string {
+	if t.HostData != nil {
+		return t.HostData.Hostname
+	}
+	return ""
 }
 
 func (t *Telemetry) SendEvent(name, data string) {
@@ -69,6 +92,7 @@ func (t *Telemetry) SendEvent(name, data string) {
 		SetBody(map[string]interface{}{
 			"version":        version.Version,
 			"telemetry_name": t.Config.Name,
+			"hostname":       t.GetHostname(),
 			"event_name":     name,
 			"data":           data,
 		}).
