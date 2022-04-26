d9eba0faf1d077eb5a15305c1850a8fd86fd7431
Ondrej Sika <ondrej@ondrejsika.com>
2022-04-27 01:28:38 +0200

    feat(backup/middleware/symmetric_encryption): Add SymmetricEncryption middleware

--- backup/middleware/middleware.go
+++ backup/middleware/middleware.go
@@ -5,15 +5,21 @@ import (
 	"io"
 
 	"github.com/sikalabs/tergum/backup/middleware/gzip"
+	"github.com/sikalabs/tergum/backup/middleware/symmetric_encryption"
 )
 
 type Middleware struct {
-	Gzip *gzip.GzipMiddleware `yaml:"Gzip"`
+	Gzip                *gzip.GzipMiddleware                                `yaml:"Gzip"`
+	SymmetricEncryption *symmetric_encryption.SymmetricEncryptionMiddleware `yaml:"SymmetricEncryption"`
 }
 
 func (m Middleware) Validate() error {
 	if m.Gzip != nil {
 		return m.Gzip.Validate()
+	}
+
+	if m.SymmetricEncryption != nil {
+		return m.SymmetricEncryption.Validate()
 	}
 
 	return fmt.Errorf("no middleware detected")
@@ -24,12 +30,20 @@ func (m Middleware) Process(data io.ReadSeeker) (io.ReadSeeker, error) {
 		return m.Gzip.Process(data)
 	}
 
+	if m.SymmetricEncryption != nil {
+		return m.SymmetricEncryption.Process(data)
+	}
+
 	return nil, fmt.Errorf("no middleware detected")
 }
 
 func (m Middleware) Name() string {
 	if m.Gzip != nil {
 		return "Gzip"
+	}
+
+	if m.SymmetricEncryption != nil {
+		return "SymmetricEncryption"
 	}
 
 	return ""
---
+++ backup/middleware/symmetric_encryption/symmetric_encryption.go
@@ -0,0 +1,68 @@
+package symmetric_encryption
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path"
+
+	"github.com/sikalabs/tergum/utils/rand_utils"
+)
+
+type SymmetricEncryptionMiddleware struct {
+	Passphrase string `yaml:"Passphrase"`
+}
+
+func (m SymmetricEncryptionMiddleware) Validate() error {
+	if m.Passphrase == "" {
+		return fmt.Errorf("SymmetricEncryptionMiddleware need to have a Passphrase")
+	}
+	return nil
+}
+
+func (m SymmetricEncryptionMiddleware) Process(data io.ReadSeeker) (io.ReadSeeker, error) {
+	// Encrypt:
+	//     gpg --batch --output greetings.txt.gpg --passphrase mypassword --symmetric greetings.txt
+	// Decrypt:
+	//     gpg --batch --output greetings1.txt --passphrase mypassword --decrypt greetings.txt.gpg
+
+	var err error
+
+	inputFile, err := os.CreateTemp("", "tergum-symmetric-encryption-input-")
+	if err != nil {
+		return nil, err
+	}
+	defer os.Remove(inputFile.Name())
+	io.Copy(inputFile, data)
+
+	outputFileName := path.Join(os.TempDir(), rand_utils.GetRandString(16))
+
+	cmd := exec.Command(
+		"gpg",
+		"--batch",
+		"--output", outputFileName,
+		"--passphrase", m.Passphrase,
+		"--symmetric", inputFile.Name(),
+	)
+	err = cmd.Start()
+	if err != nil {
+		return nil, err
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	outputFile, err := os.Open(outputFileName)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	outputFile.Seek(0, 0)
+
+	return outputFile, nil
+}
