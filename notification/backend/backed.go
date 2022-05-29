53cdcb1be1df231a524476448805fb5c10135633
Ondrej Sika <ondrej@ondrejsika.com>
2022-05-29 13:02:47 +0200

    fix(notification/backed): Make backend configuration optional (due to another (slack) target)

--- notification/backend/backed.go
+++ notification/backend/backed.go
@@ -3,13 +3,15 @@ package backend
 import "github.com/sikalabs/tergum/notification/backend/email"
 
 type Backend struct {
-	Email email.EmailBackend `yaml:"Email"`
+	Email *email.EmailBackend `yaml:"Email"`
 }
 
 func (b Backend) Validate() error {
-	err := b.Email.Validate()
-	if err != nil {
-		return err
+	if b.Email != nil {
+		err := b.Email.Validate()
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
