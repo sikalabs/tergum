2f24d2ff1ddb67bb4643315536fde8336da3beed
Ondrej Sika <ondrej@ondrejsika.com>
2022-05-29 09:16:04 +0200

    feat(backup/backup_process): Add Exec method to BackupProcess to backup using exec

--- backup/backup_process/backup_process.go
+++ backup/backup_process/backup_process.go
@@ -2,15 +2,29 @@ package backup_process
 
 import (
 	"fmt"
+	"io"
+	"os"
+	"os/exec"
 	"strings"
 )
 
 type BackupProcess struct {
 	StderrBuff *strings.Builder
+	Data       io.ReadWriteSeeker
 }
 
 func (bp *BackupProcess) Init() {
 	bp.StderrBuff = new(strings.Builder)
+}
+
+func (bp *BackupProcess) InitDataTempFile() error {
+	f, err := os.CreateTemp("", "tergum-dump-mysql-")
+	if err != nil {
+		return err
+	}
+	defer os.Remove(f.Name())
+	bp.Data = f
+	return nil
 }
 
 func (bp BackupProcess) GetStderr() (string, error) {
@@ -18,4 +32,40 @@ func (bp BackupProcess) GetStderr() (string, error) {
 		return "", fmt.Errorf("StderrBuff is nil")
 	}
 	return bp.StderrBuff.String(), nil
+}
+
+func (bp *BackupProcess) GetData() (io.ReadSeeker, error) {
+	_, err := bp.Data.Seek(0, 0)
+	return bp.Data, err
+}
+
+func (bp *BackupProcess) GetDataStderr() (io.ReadSeeker, string, error) {
+	data, err := bp.GetData()
+	if err != nil {
+		return nil, "", err
+	}
+	stderr, err := bp.GetStderr()
+	if err != nil {
+		return nil, "", err
+	}
+	return data, stderr, nil
+}
+
+func (bp *BackupProcess) ExecWait(bin string, args ...string) error {
+	var err error
+
+	cmd := exec.Command(bin, args...)
+	cmd.Stdout = bp.Data
+	cmd.Stderr = bp.StderrBuff
+
+	err = cmd.Start()
+	if err != nil {
+		return err
+	}
+	err = cmd.Wait()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
