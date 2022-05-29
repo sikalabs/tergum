88dbcd894594cd8bf734a4d11df227ec7df564b6
Ondrej Sika <ondrej@ondrejsika.com>
2022-05-29 11:41:32 +0200

    feat(backup_process_utils): Add util BackupProcessExecEnvToFile

--- backup_process_utils/backup_process_utils.go
+++ backup_process_utils/backup_process_utils.go
@@ -24,3 +24,23 @@ func BackupProcessExecToFile(bin string, args ...string) (io.ReadSeeker, string,
 	}
 	return bp.GetDataStderr()
 }
+
+func BackupProcessExecEnvToFile(env []string, bin string, args ...string) (io.ReadSeeker, string, error) {
+	var err error
+	bp := backup_process.BackupProcess{}
+	bp.Init()
+	err = bp.InitDataTempFile()
+	if err != nil {
+		return nil, "", err
+	}
+	err = bp.ExecEnvWait(
+		env,
+		bin,
+		args...,
+	)
+	if err != nil {
+		stderr, _ := bp.GetStderr()
+		return nil, stderr, err
+	}
+	return bp.GetDataStderr()
+}
