0ad4de18fb56d66ffa6db01705e83635d4a89651
Ondrej Sika <ondrej@ondrejsika.com>
2022-05-29 10:57:33 +0200

    refactor(backup/source/kubernetes): Backup using BackupProcessExecToFile helper

--- backup/source/kubernetes/kubernetes.go
+++ backup/source/kubernetes/kubernetes.go
@@ -3,9 +3,8 @@ package kubernetes
 import (
 	"fmt"
 	"io"
-	"os"
-	"os/exec"
-	"strings"
+
+	"github.com/sikalabs/tergum/backup/backup_process_utils"
 )
 
 type Kubernetes struct {
@@ -30,15 +29,6 @@ func (s Kubernetes) Validate() error {
 }
 
 func (s Kubernetes) Backup() (io.ReadSeeker, string, error) {
-	var err error
-	errorMessage := new(strings.Builder)
-
-	outputFile, err := os.CreateTemp("", "tergum-dump-k8s-")
-	if err != nil {
-		return nil, "", err
-	}
-	defer os.Remove(outputFile.Name())
-
 	args := []string{
 		"--server", s.Server,
 		"--insecure-skip-tls-verify=true",
@@ -56,22 +46,8 @@ func (s Kubernetes) Backup() (io.ReadSeeker, string, error) {
 		args = append(args, "--all-namespaces")
 	}
 
-	cmd := exec.Command(
+	return backup_process_utils.BackupProcessExecToFile(
 		"kubectl",
 		args...,
 	)
-	cmd.Stdout = outputFile
-	cmd.Stderr = errorMessage
-
-	err = cmd.Start()
-	if err != nil {
-		return nil, errorMessage.String(), err
-	}
-	err = cmd.Wait()
-	if err != nil {
-		return nil, errorMessage.String(), err
-	}
-
-	outputFile.Seek(0, 0)
-	return outputFile, "", nil
 }
