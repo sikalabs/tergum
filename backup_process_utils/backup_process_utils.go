1a312c7154acedae001e739eca350642e829f088
Ondrej Sika <ondrej@ondrejsika.com>
2022-07-29 10:32:58 +0200

    feat(backup_process_utils): Add BackupProcessHttpGetWithToken helper (for Vault backups)

--- backup_process_utils/backup_process_utils.go
+++ backup_process_utils/backup_process_utils.go
@@ -1,6 +1,10 @@
 package backup_process_utils
 
 import (
+	"fmt"
+	"io"
+	"net/http"
+
 	"github.com/sikalabs/tergum/backup_output"
 	"github.com/sikalabs/tergum/backup_process"
 )
@@ -48,4 +52,53 @@ func BackupProcessExecEnvToFile(env []string, bin string, args ...string) (backu
 		return bo, err
 	}
 	return bp.GetDataStderr()
+}
+
+func BackupProcessHttpGetWithToken(
+	url string,
+	tokenHeaderName string,
+	tokenHeaderValue string,
+) (backup_output.BackupOutput, error) {
+	var err error
+	var bo backup_output.BackupOutput
+
+	bp := backup_process.BackupProcess{}
+	bp.Init()
+	err = bp.InitDataTempFile()
+	if err != nil {
+		return bo, err
+	}
+
+	body, err := httpGetWithToken(url, tokenHeaderName, tokenHeaderValue)
+	if err != nil {
+		io.Copy(bp.StderrBuff, body)
+		bo, _ = bp.GetDataStderr()
+		return bo, err
+	}
+
+	io.Copy(bp.Data, body)
+	return bp.GetDataStderr()
+}
+
+func httpGetWithToken(
+	url string,
+	tokenHeaderName string,
+	tokenHeaderValue string,
+) (io.ReadCloser, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set(tokenHeaderName, tokenHeaderValue)
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != 200 {
+		return resp.Body, fmt.Errorf("http status code is %d", resp.StatusCode)
+	}
+
+	return resp.Body, nil
 }
