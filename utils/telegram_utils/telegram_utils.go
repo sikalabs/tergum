e291f6bd29597cb83dc55c3a8b621b0ef9e1381a
Ondrej Sika <ondrej@ondrejsika.com>
2025-05-08 09:51:19 +0200

    feat(utils/telegram_utils): Create func TelegramGetLastChatID

--- utils/telegram_utils/telegram_utils.go
+++ utils/telegram_utils/telegram_utils.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -23,4 +24,46 @@ func TelegramSendMessage(botToken string, chatID int64, message string) error {
 	defer resp.Body.Close()
 
 	return nil
+}
+
+func TelegramGetLastChatID(botToken string) (int64, error) {
+	type Chat struct {
+		ID int64 `json:"id"`
+	}
+
+	type Message struct {
+		Chat Chat `json:"chat"`
+	}
+
+	type Update struct {
+		Message Message `json:"message"`
+	}
+
+	type Response struct {
+		Ok     bool     `json:"ok"`
+		Result []Update `json:"result"`
+	}
+
+	resp, err := http.Get("https://api.telegram.org/bot" + botToken + "/getUpdates")
+	if err != nil {
+		return 0, fmt.Errorf("getting updates failed: %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return 0, fmt.Errorf("reading response body failed: %v", err)
+	}
+
+	var response Response
+	if err := json.Unmarshal(body, &response); err != nil {
+		return 0, fmt.Errorf("unmarshalling response failed: %v", err)
+	}
+
+	if len(response.Result) == 0 {
+		return 0, fmt.Errorf("no updates found")
+	}
+
+	lastChatID := response.Result[len(response.Result)-1].Message.Chat.ID
+	return lastChatID, nil
 }
