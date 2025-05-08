c00ccee8ce90b5b0f17f57efb9f8193d04ad1e1f
Ondrej Sika <ondrej@ondrejsika.com>
2025-05-08 09:55:39 +0200

    feat(notification/target/telegram): Add Telegram as notification target

--- notification/target/target.go
+++ notification/target/target.go
@@ -7,12 +7,14 @@ import (
 	"github.com/sikalabs/tergum/notification/backend"
 	"github.com/sikalabs/tergum/notification/target/email"
 	"github.com/sikalabs/tergum/notification/target/slack_webhook"
+	"github.com/sikalabs/tergum/notification/target/telegram"
 )
 
 type Target struct {
 	OnErrorOnly  bool                        `yaml:"OnErrorOnly" json:"OnErrorOnly,omitempty"`
 	Email        *email.EmailRule            `yaml:"Email" json:"Email,omitempty"`
 	SlackWebhook *slack_webhook.SlackWebhook `yaml:"SlackWebhook" json:"SlackWebhook,omitempty"`
+	Telegram     *telegram.Telegram          `yaml:"Telegram" json:"Telegram,omitempty"`
 }
 
 func (r Target) Validate() error {
@@ -26,6 +28,14 @@ func (r Target) Validate() error {
 
 	if r.SlackWebhook != nil {
 		err := r.SlackWebhook.Validate()
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	if r.Telegram != nil {
+		err := r.Telegram.Validate()
 		if err != nil {
 			return err
 		}
@@ -49,6 +59,14 @@ func (r Target) SendNotification(
 
 	if r.SlackWebhook != nil {
 		err := r.SlackWebhook.SendNotification(bl, b)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	if r.Telegram != nil {
+		err := r.Telegram.SendNotification(bl, b)
 		if err != nil {
 			return err
 		}
---
+++ notification/target/telegram/telegram.go
@@ -0,0 +1,57 @@
+package telegram
+
+import (
+	"fmt"
+
+	"github.com/sikalabs/tergum/backup_log"
+	"github.com/sikalabs/tergum/backup_log/backup_log/output"
+	"github.com/sikalabs/tergum/notification/backend"
+	"github.com/sikalabs/tergum/utils/telegram_utils"
+)
+
+type Telegram struct {
+	BotToken string `yaml:"BotToken" json:"BotToken,omitempty"`
+	ChatID   int64  `yaml:"ChatID" json:"ChatID,omitempty"`
+	SendOK   bool   `yaml:"SendOK" json:"SendOK,omitempty"`
+}
+
+func (t Telegram) Validate() error {
+	if t.BotToken == "" {
+		return fmt.Errorf("telegram: BotToken is required")
+	}
+	if t.ChatID == 0 {
+		return fmt.Errorf("telegram: ChatID is required")
+	}
+	return nil
+}
+
+func (t Telegram) SendNotification(
+	bl backup_log.BackupLog,
+	b backend.Backend,
+) error {
+	// Skip sending email on successfull backups
+	// if SendOK is not set or set to false
+	if bl.GlobalSuccess() && !t.SendOK {
+		logSkipped()
+		return nil
+	}
+	table := output.BackupLogToTelegramString(bl)
+	errorTable := output.BackupErrorLogToTelegramString(bl)
+
+	text := bl.GlobalSuccessEmoji() +
+		bl.GlobalSuccessEmoji() +
+		bl.GlobalSuccessEmoji() +
+		"\n\n" + table + "\n" + errorTable + "\n"
+
+	err := telegram_utils.TelegramSendMessage(
+		t.BotToken,
+		t.ChatID,
+		text,
+	)
+	if err != nil {
+		return fmt.Errorf("telegram: %w", err)
+	}
+
+	logSend()
+	return nil
+}
