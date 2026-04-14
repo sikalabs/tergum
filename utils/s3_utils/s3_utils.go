9908448fddffb45695321547257001717ea6312b
Vojtěch Pastyřík <vojtech.pastyrik@sikalabs.com>
2026-04-14 12:23:43 +0200

    feat(utils/s3_utils): add retention policy for automatic deletion of old backups

--- utils/s3_utils/s3_utils.go
+++ utils/s3_utils/s3_utils.go
@@ -7,6 +7,7 @@ import (
 	aws_aws "github.com/aws/aws-sdk-go/aws"
 	aws_credentials "github.com/aws/aws-sdk-go/aws/credentials"
 	aws_session "github.com/aws/aws-sdk-go/aws/session"
+	aws_s3 "github.com/aws/aws-sdk-go/service/s3"
 	aws_s3manager "github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/cheggaaa/pb/v3"
 )
@@ -65,6 +66,104 @@ func Upload(
 	}
 
 	bar.Finish()
+
+	return nil
+}
+
+func newSession(access_key, secret_key, region, endpoint string) (*aws_session.Session, error) {
+	awsConfig := aws_aws.Config{
+		Credentials: aws_credentials.NewStaticCredentials(
+			access_key,
+			secret_key,
+			"",
+		),
+	}
+	if region != "" {
+		awsConfig.Region = aws_aws.String(region)
+	}
+	if endpoint != "" {
+		awsConfig.Region = aws_aws.String("us-east-1")
+		awsConfig.S3ForcePathStyle = aws_aws.Bool(true)
+		awsConfig.Endpoint = aws_aws.String(endpoint)
+	}
+	return aws_session.NewSession(&awsConfig)
+}
+
+func ListObjects(
+	access_key string,
+	secret_key string,
+	region string,
+	endpoint string,
+	bucket_name string,
+	prefix string,
+) ([]*aws_s3.Object, error) {
+	session, err := newSession(access_key, secret_key, region, endpoint)
+	if err != nil {
+		return nil, err
+	}
+	svc := aws_s3.New(session)
+
+	var objects []*aws_s3.Object
+	input := &aws_s3.ListObjectsV2Input{
+		Bucket: aws_aws.String(bucket_name),
+		Prefix: aws_aws.String(prefix),
+	}
+
+	err = svc.ListObjectsV2Pages(input, func(page *aws_s3.ListObjectsV2Output, lastPage bool) bool {
+		objects = append(objects, page.Contents...)
+		return true
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return objects, nil
+}
+
+func DeleteObjects(
+	access_key string,
+	secret_key string,
+	region string,
+	endpoint string,
+	bucket_name string,
+	keys []string,
+) error {
+	if len(keys) == 0 {
+		return nil
+	}
+
+	session, err := newSession(access_key, secret_key, region, endpoint)
+	if err != nil {
+		return err
+	}
+	svc := aws_s3.New(session)
+
+	// S3 DeleteObjects API allows max 1000 keys per request
+	for i := 0; i < len(keys); i += 1000 {
+		end := i + 1000
+		if end > len(keys) {
+			end = len(keys)
+		}
+
+		batch := keys[i:end]
+		objectIds := make([]*aws_s3.ObjectIdentifier, len(batch))
+		for j, key := range batch {
+			objectIds[j] = &aws_s3.ObjectIdentifier{
+				Key: aws_aws.String(key),
+			}
+		}
+
+		_, err := svc.DeleteObjects(&aws_s3.DeleteObjectsInput{
+			Bucket: aws_aws.String(bucket_name),
+			Delete: &aws_s3.Delete{
+				Objects: objectIds,
+				Quiet:   aws_aws.Bool(true),
+			},
+		})
+		if err != nil {
+			return err
+		}
+	}
 
 	return nil
 }
