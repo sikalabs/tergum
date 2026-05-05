6d4ceb72d9df6c04780c3ca224df1ce63bf2476f
Ondrej Sika <ondrej@ondrejsika.com>
2026-05-05 13:29:18 +0200

    feat(backup/source/vault): Add support of Vault token from Kubernetes SA Token

--- backup/source/vault/vault.go
+++ backup/source/vault/vault.go
@@ -2,10 +2,13 @@ package vault
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/sikalabs/tergum/backup_output"
 	"github.com/sikalabs/tergum/backup_process_utils"
 )
+
+const k8sSATokenPath = "/var/run/secrets/kubernetes.io/serviceaccount/token"
 
 type VaultSource struct {
 	Addr    string            `yaml:"Addr" json:"Addr,omitempty"`
@@ -13,21 +16,38 @@ type VaultSource struct {
 	Headers map[string]string `yaml:"Headers" json:"Headers,omitempty"`
 }
 
+func (s VaultSource) getToken() (string, error) {
+	if s.Token != "" {
+		return s.Token, nil
+	}
+	data, err := os.ReadFile(k8sSATokenPath)
+	if err != nil {
+		return "", fmt.Errorf("VaultSource requires a Token or a Kubernetes SA token at %s", k8sSATokenPath)
+	}
+	return string(data), nil
+}
+
 func (s VaultSource) Validate() error {
 	if s.Addr == "" {
 		return fmt.Errorf("VaultSource need to have a Addr")
 	}
-	if s.Token == "" {
-		return fmt.Errorf("VaultSource need to have a Token")
+	// Try to get token from config or Kubernetes SA
+	_, err := s.getToken()
+	if err != nil {
+		return err
 	}
 	return nil
 }
 
 func (s VaultSource) Backup() (backup_output.BackupOutput, error) {
+	token, err := s.getToken()
+	if err != nil {
+		return backup_output.BackupOutput{}, err
+	}
 	return backup_process_utils.BackupProcessHttpGetWithToken(
 		s.Addr+"/v1/sys/storage/raft/snapshot",
 		"X-Vault-Token",
-		s.Token,
+		token,
 		s.Headers,
 	)
 }
